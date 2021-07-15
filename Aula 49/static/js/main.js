4a56200cc697f18bb1a01d7f2b1b043238f79f91
william-cirico <contato.williamc@gmail.com>
2021-07-15 01:56:48 -0300

    Aula 49

---
+++ Aula 49/static/js/main.js
@@ -0,0 +1,8 @@
+const jsonCards = "[{\"name\":\"Serra Angel\",\"image\":\"static/img/serraangel.png\",\"manaCost\":3,\"typeLine\":\"Creature - Angel\",\"expansionSymbol\":\"static/img/serraangelexpansionsymbol.png\",\"hability\":\"Flying, vigilance\",\"text\":\"The angel remembers her past lives like dreams. Her song held up meadows. Her blade darkness. Her wings carried her across the ages.\",\"power\":\"4/4\",\"collectorNumber\":\"033/269 U\"},{\"name\":\"Brainstorm\",\"image\":\"\",\"manaCost\":4,\"typeLine\":\"Instant\",\"expansionSymbol\":\"static/img/kokushoexpansionsymbol.png\",\"hability\":\"Flying<br>When Kokusho, the Evening Star dies, each opponent loses 5 life. You gain life equal to the life lost this way.\",\"text\":\"The fall of the evening star never heralds a gentle dawn.\",\"power\":\"5/5\",\"collectorNumber\":\"095/249 R\"}]"   
+
+/* 
+Exercício 
+
+Utilizando grid e flexbox reproduza os cards de magic com as informações do jsonCards.
+Para isso converta o json para um vetor e adicione cada item como uma carta.
+*/
